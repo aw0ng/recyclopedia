ea791ad0fe4c67d61d525f461bd35c05e349d84b
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-18 17:37:15 -0500

    seed donors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Donor.create(email: "anna@test.com", donor_name: "Anna", donor_surname: "Wong")
+Donor.create(email: "lexa@test.com", donor_name: "Lexa", donor_surname: "Wong")
+Donor.create(email: "harry@test.com", donor_name: "Harry", donor_surname: "Abrahamsen")
+Donor.create(email: "tara@test.com", donor_name: "Tara", donor_surname: "Charnow")
+Donor.create(email: "adara@test.com", donor_name: "Adara", donor_surname: "Gemellaro")
+Donor.create(email: "joey@test.com", donor_name: "Joey", donor_surname: "Gemellaro")
+
