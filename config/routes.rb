8a4f391e8cab2c4ba96f7617d32b167a3cca059d
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-20 20:23:38 -0500

    add donor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/donors" => "donors#index"
+    post "/donors" => "donors#create"
+    get "/donors/:id" => "donors#show"
+    patch "/donors/:id" => "donors#update"
+    delete "/donors/:id" => "donors#destroy"
+  end
 end
