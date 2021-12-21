780319433305577b076de73e7056ec7661ee0506
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-20 21:32:07 -0500

    add contoller actions for donors

--- app/controllers/api/donors_controller.rb
+++ app/controllers/api/donors_controller.rb
@@ -1,2 +1,36 @@
 class Api::DonorsController < ApplicationController
+  def index
+    @donors = Donor.all
+    render "index.json.jb"
+  end
+
+  def create
+    @donor = Donor.new(
+      email: params[:email],
+      donor_name: params[:donor_name],
+      donor_surname: params[:donor_surname]
+    )
+    @donor.save
+    render "show.json.jb"
+  end
+
+  def show
+    @donor = Donor.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @donor = Donor.find_by(id: params[:id])
+    @donor.email = params[:email] || @donor.email
+    @donor.donor_name = params[:donor_name] || @donor.donor_name
+    @donor.donor_surname = params[:donor_surname] || @donor.donor_surname
+    @donor.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    donor = Donor.find_by(id: params[:id])
+    donor.destroy
+    render json: {message: "Donor successfully destroyed."}
+  end
 end
