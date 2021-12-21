80dc1e6c5c98c6b270ab38914258f61e3ed92f7d
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-20 21:23:46 -0500

    add item routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
     get "/donors/:id" => "donors#show"
     patch "/donors/:id" => "donors#update"
     delete "/donors/:id" => "donors#destroy"
+    
+    get "/items" => "items#index"
+    post "/items" => "items#create"
+    get "/items/:id" => "items#show"
+    patch "/items/:id" => "items#update"
+    delete "/items/:id" => "items#destroy"
   end
 end
