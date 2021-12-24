b7e385cc7beaa3ba6ff66f790ccd67b69ae62b33
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-12-23 20:23:37 -0500

    Add Image to Items migration

---
+++ db/migrate/20211224011949_add_imageto_items.rb
@@ -0,0 +1,5 @@
+class AddImagetoItems < ActiveRecord::Migration[6.1]
+  def change
+    add_column :items, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_045424) do
+ActiveRecord::Schema.define(version: 2021_12_24_011949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_045424) do
     t.integer "donor_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "receivers", force: :cascade do |t|
